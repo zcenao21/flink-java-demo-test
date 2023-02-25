66983dd246e085609966001299e49166ccc05d46
zcenao21 <zcenao21@gmail.com>
2023-02-25 15:47:32 +0800

    format code

--- src/main/java/com/will/stream/timer/TimerTest.java
+++ src/main/java/com/will/stream/timer/TimerTest.java
@@ -1,12 +1,9 @@
 package com.will.stream.timer;
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import java.text.SimpleDateFormat;
 
 public class TimerTest {
-    static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
@@ -18,7 +15,7 @@ public class TimerTest {
         source.print();
 
         source
-            .keyBy((event)->event.stockName)
+            .keyBy((event) -> event.stockName)
             .process(new StockProcessFunction())
             .print();
 
